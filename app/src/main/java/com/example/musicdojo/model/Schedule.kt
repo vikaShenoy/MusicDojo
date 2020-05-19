69a926f921bc25511534fa1e20e486101df97333
vsh33 <vsh33@uclive.ac.nz>
2020-05-19 16:52:53 +1200

    Add dependencies for Room

--- app/src/main/java/com/example/musicdojo/model/Schedule.kt
+++ app/src/main/java/com/example/musicdojo/model/Schedule.kt
@@ -1,8 +1,28 @@
 package com.example.musicdojo.model
 
+import androidx.room.*
+
+@Entity(tableName = "schedules")
 class Schedule(
     val name: String = "",
     val exercises: List<Exercise>? = null,
     val completed: Boolean = false
 ) {
+    @PrimaryKey(autoGenerate = true)
+    var id : Long = 0
+}
+
+@Dao
+interface ScheduleDao {
+    @Insert
+    fun insert(schedule: Schedule): Long
+
+    @Update
+    fun update(schedule: Schedule)
+
+    @Query("SELECT * FROM schedules")
+    fun getAll(): List<Schedule>
+
+    @Delete
+    fun delete(schedule: Schedule)
 }
